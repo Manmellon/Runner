52524056093b442fd25814311f0ea03002632777
loliallen <maxim.kasakin@yahoo.com>
2021-12-09 01:14:27 +0300

    update lmcd checling

--- socket.js
+++ socket.js
@@ -8,26 +8,51 @@ function gDO(event, data) {
     })
 }
 
+function loginUser() {
+    return new Promise((res, rej) => {
+        const user = mcd.bridge.message('user');
+        let outData = {}
+        user.send({ "promptlogin": true });
+
+        user.on('data', function (data) {
+            // myGameInstance.SendMessage('VoucherPanel', 'Callback', gDO('user_data', data))
+            outData = data
+        });
+        user.on('error', function (error) {
+            // myGameInstance.SendMessage('VoucherPanel', 'Callback', gDO('user_data', error))
+            rej(error, outData)
+        });
+        user.on('done', function () {
+            // myGameInstance.SendMessage('VoucherPanel', 'Callback', gDO('user_done'))
+            res(outData)
+        });
+    })
+}
 
 
-function activateOffer(itemId) {
+async function activateOffer(itemId) {
     if (!myGameInstance)
         return console.error('myGameInstance isn\'t defined')
     if (!isMcdDefined)
-        return myGameInstance.SendMessage('VoucherPanel', 'Callback', gDO('error', 'mcd isb\'t defined'))
-
-    var offerActivation = mcd.bridge.message('offerActivation');
-    offerActivation.send({
-        'loyaltyId': 2919,
-        'rewardId': itemId
-    });
-    offerActivation.on('data', (data) => {
-        myGameInstance.SendMessage('VoucherPanel', 'Callback', gDO('data', data))
-    });
-    offerActivation.on('error', (error) => {
-        myGameInstance.SendMessage('VoucherPanel', 'Callback', gDO('data', error))
-    });
-    offerActivation.on('done', () => {
-        myGameInstance.SendMessage('VoucherPanel', 'Callback', gDO('done'))
-    });
+        return myGameInstance.SendMessage('VoucherPanel', 'Callback', gDO('error', 'mcd isn\'t defined'))
+    loginUser()
+        .then(data => {
+            myGameInstance.SendMessage('VoucherPanel', 'Callback', gDO('data', { u: data }))
+            var offerActivation = mcd.bridge.message('offerActivation');
+            offerActivation.send({
+                'loyaltyId': 2919,
+                'rewardId': itemId
+            });
+            offerActivation.on('data', (data) => {
+                myGameInstance.SendMessage('VoucherPanel', 'Callback', gDO('data', data))
+            });
+            offerActivation.on('error', (error) => {
+                myGameInstance.SendMessage('VoucherPanel', 'Callback', gDO('data', error))
+            });
+            offerActivation.on('done', () => {
+                myGameInstance.SendMessage('VoucherPanel', 'Callback', gDO('done'))
+            });
+        }).catch(e => {
+            myGameInstance.SendMessage('VoucherPanel', 'Callback', gDO('error', { u: e }))
+        })
 }
